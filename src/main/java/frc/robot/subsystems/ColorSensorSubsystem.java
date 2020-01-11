51a888898830d2f6b79fd5e4b214be59245afa13
Geoffry Appelbaum <appelbaumgl@msoe.edu>
2020-01-11 11:03:18 -0600

    Removed unused imports.

--- src/main/java/frc/robot/subsystems/ColorSensorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ColorSensorSubsystem.java
@@ -10,10 +10,8 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import edu.wpi.first.wpilibj.I2C;
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.util.Color;
-import frc.robot.commands.ColorSensorCommand;
 
 import com.revrobotics.ColorSensorV3;
 import com.revrobotics.ColorMatchResult;
