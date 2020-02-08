ea7f8e12757ab45dee25e1d9dc90b6346dc9d47c
unknown <portpiratech@gmail.com>
2020-02-08 09:28:16 -0600

    updated

--- src/main/java/frc/robot/commands/CameraCommand.java
+++ src/main/java/frc/robot/commands/CameraCommand.java
@@ -7,9 +7,9 @@
 
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj.GenericHID.RumbleType;
+// import edu.wpi.first.wpilibj.GenericHID.RumbleType;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.OI;
+// import frc.robot.OI;
 import frc.robot.Robot;
 
 public class CameraCommand extends CommandBase {
